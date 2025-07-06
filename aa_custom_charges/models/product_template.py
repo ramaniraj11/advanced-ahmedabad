80a10d964a80767727f6d470d0592a396222d99c
Ramani Raj <ramaniraj@MacBookAir.lan>
2025-07-06 21:53:07 +0530

    aa

--- aa_custom_charges/models/product_template.py
+++ aa_custom_charges/models/product_template.py
@@ -4,7 +4,6 @@ from odoo import fields, models, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    hsn_code = fields.Char(string="HSN Code")
     part_number = fields.Char(string="Part Number")
     item_make = fields.Char(string="Item Make")
     model_number = fields.Char(string="Model Number")
